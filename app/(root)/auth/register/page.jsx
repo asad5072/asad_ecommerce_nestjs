658b09b747fae3d74e6f6c8e98d258fcc0ff0864
asad5072 <mmonir5072@gmail.com>
2025-12-31 18:26:16 +0600

    register

--- app/(root)/auth/register/page.jsx
+++ app/(root)/auth/register/page.jsx
@@ -12,19 +12,37 @@ import { FaRegEyeSlash } from "react-icons/fa";
 import { FaRegEye } from "react-icons/fa6";
 import { useState } from "react";
 
-const Login = () => {
+const Register = () => {
     const form = useForm();
     const onSubmit = (data) => {
         console.log(data);
     }
     const [isTypePassword, setIsTypePassword] = useState(true);
+    const [isConfirmTypePassword, setIsConfirmTypePassword] = useState(true);
     return (
       <Card className="w-full max-w-sm m-auto">        
         <CardContent>
           <p className="text-2xl font-bold text-center">Logo</p>
-          <p className="text-center mt-4">Log in to your account!</p>
+          <p className="text-center mt-4">Create an account!</p>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+							control={form.control}
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input
+											type="text"
+											placeholder="John Doe"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
               <FormField
                 control={form.control}
                 name="email"
@@ -33,10 +51,10 @@ const Login = () => {
                     <FormLabel>email</FormLabel>
                     <FormControl>
                       <Input
-													type="email"
-													placeholder="example@gmail.com"
-													{...field}
-												/>
+                          type="email"
+                          placeholder="example@gmail.com"
+                          {...field}
+                        />
                     </FormControl>                   
                     <FormMessage />
                   </FormItem>
@@ -52,31 +70,57 @@ const Login = () => {
                       <Input placeholder="......" {...field} type={isTypePassword ? "password" : "text"} />
                     </FormControl>
                     <button
-												type="button"
-												className="absolute top-1/2 right-2 cursor-pointer"
-												onClick={() => setIsTypePassword(!isTypePassword)}
-											>
-												{isTypePassword ? <FaRegEyeSlash /> : <FaRegEye />}
-											</button>
+                        type="button"
+                        className="absolute top-1/2 right-2 cursor-pointer"
+                        onClick={() => setIsTypePassword(!isTypePassword)}
+                      >
+                        {isTypePassword ? <FaRegEyeSlash /> : <FaRegEye />}
+                      </button>
                     <FormMessage />
                   </FormItem>
                 )}
               />
-              <Button type="submit" className="w-full">Login</Button>
+              <FormField
+							control={form.control}
+							name="confirmPassword"
+							render={({ field }) => (
+								<FormItem className="relative">
+									<FormLabel>Confirm Password</FormLabel>
+									<FormControl>
+										<Input
+											type={isConfirmTypePassword ? "password" : "text"}
+											placeholder="•••••••"
+											{...field}
+										/>
+									</FormControl>
+									<button
+										type="button"
+										className="absolute top-1/2 right-2 cursor-pointer"
+										onClick={() =>
+											setIsConfirmTypePassword(!isConfirmTypePassword)
+										}
+									>
+										{isConfirmTypePassword ? <FaRegEyeSlash /> : <FaRegEye />}
+									</button>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+              <Button type="submit" className="w-full">Create Account</Button>
             </form>
           </Form>
           <div className="flex items-center justify-center gap-2">
-            <p>Do not have a account?</p>
+            <p>Already have a account?</p>
             <Link
               className="text-primary text-sm underline"
               href="#"
             >
-              Create an Account
+              Login
             </Link>
-				  </div>
+          </div>
         </CardContent>        
       </Card>
     )
 }
 
-export default Login
+export default Register
